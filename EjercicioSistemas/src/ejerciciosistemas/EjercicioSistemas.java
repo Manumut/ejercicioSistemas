0088a3f1b7d5c139b801103c157b91e0149a7dd4
unknown <alex.chinchi02@gmail.com>
2024-05-21 18:36:51 +0200

    Hola esto es un cambio

---
+++ EjercicioSistemas/src/ejerciciosistemas/EjercicioSistemas.java
@@ -0,0 +1,12 @@
+package ejerciciosistemas;
+
+import java.util.Scanner;
+
+public class EjercicioSistemas {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Hola!!");
+    }
+    
+}
