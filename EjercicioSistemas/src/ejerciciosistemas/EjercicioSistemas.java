a3be763b08f165b53956ba08aaf9d531abce0a92
unknown <alex.chinchi02@gmail.com>
2024-05-21 18:49:54 +0200

    Modificaciones

--- EjercicioSistemas/src/ejerciciosistemas/EjercicioSistemas.java
+++ EjercicioSistemas/src/ejerciciosistemas/EjercicioSistemas.java
@@ -7,6 +7,7 @@ public class EjercicioSistemas {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Hola!!");
+        System.out.println("Esto es una modificacion");
     }
     
 }
